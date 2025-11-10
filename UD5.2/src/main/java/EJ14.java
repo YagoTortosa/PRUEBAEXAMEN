60d8c5d61948fff76422c5b37a83af4e6a16019d
YAGO <yagotortosa72@gmail.com>
2025-11-10 20:50:16 +0100

    Changes

--- UD5.2/src/main/java/EJ14.java
+++ UD5.2/src/main/java/EJ14.java
@@ -4,18 +4,39 @@ public class EJ14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        final int billete500 = 500;
-        final int billete200 = 200;
-        final int billete100 = 100;
-        final int billete50 = 50;
-        final int billete20 = 20;
-        final int billete10 = 10;
-        final int billete5 = 5;
+         int billete500 = 0;
+         int billete200 = 0;
+         int billete100 = 0;
+         int billete50 = 0;
+         int billete20 = 0;
+         int billete10 = 0;
+         int billete5 = 0;
 
         System.out.print("Introduce un cantidad: ");
         int cantidad = sc.nextInt();
 
+        if (cantidad % 5 == 0) {
+            if (cantidad / 500 == 0) {
+
+                billete500++;
+            } else if (cantidad / 200 == 0) {
+                billete200++;
+            } else if (cantidad / 100 == 0) {
+                billete100++;
+            } else if (cantidad / 50 == 0) {
+                billete50++;
+            } else if (cantidad / 20 == 0) {
+                billete20++;
+            } else if (cantidad / 10 == 0) {
+                billete10++;
+            } else if (cantidad / 5 == 0) {
+                billete5++;
+            }
+        } else {
+            System.out.println("La cantidad debe ser mayor que 5");
+        }
 
 
     }
+
 }
