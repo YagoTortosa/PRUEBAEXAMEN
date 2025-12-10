1dcecf2c21a786a832529dd43d90d8c8f00fce43
YAGO <yagotortosa72@gmail.com>
2025-12-10 13:09:50 +0100

    kk

--- UD6_1/src/main/java/UD6_MATRICES/EJ8.java
+++ UD6_1/src/main/java/UD6_MATRICES/EJ8.java
@@ -3,16 +3,55 @@ package UD6_MATRICES;
 public class EJ8 {
     public static void main(String[] args) {
 
-        final String[] PAISES = new String[] {"España", "Alemania", "Francia", "Italia"};
+        final String[] PAISES = {"España", "Alemania", "Francia", "Italia"};
 
-        final int ESTATURAMINIMA = 140;
-        final int ESTATURAMAXIMA = 210;
+        final int ESTATURA_MINIMA = 140;
+        final int ESTATURA_MAXIMA = 210;
+        final int ESTATURAS_DEPORTISTAS = 10;
+
+        final int[][] ESTATURAS = new int[4][ESTATURAS_DEPORTISTAS];
+
+        int anchoPais = 0;
+        for (int i = 0; i < PAISES.length; i++) {
+            if (PAISES[i].length() > anchoPais) {
+                anchoPais = PAISES[i].length();
+            }
+        }
+
+        anchoPais += 2;
 
 
-        PAISES[0] = String.valueOf(Math.random() * ESTATURAMAXIMA) + ESTATURAMINIMA;
-        PAISES[1] = String.valueOf(Math.random() * ESTATURAMAXIMA) + ESTATURAMINIMA;
-        PAISES[2] = String.valueOf(Math.random() * ESTATURAMAXIMA) + ESTATURAMINIMA;
-        PAISES[3] = String.valueOf(Math.random() * ESTATURAMAXIMA) + ESTATURAMINIMA;
+        int anchoEstaturas = ESTATURAS_DEPORTISTAS * 4;
+        int totalEspacios = anchoPais + anchoEstaturas + 2;
+        System.out.printf("%" + totalEspacios + "s  MED MIN MAX%n", "");
 
+
+        for (int pais = 0; pais < PAISES.length; pais++) {
+
+            System.out.printf("%" + anchoPais + "s: ", PAISES[pais]);
+
+            int suma = 0;
+            int minima = ESTATURA_MAXIMA;
+            int maxima = ESTATURA_MINIMA;
+
+            for (int i = 0; i < ESTATURAS_DEPORTISTAS; i++) {
+
+                int aleatorio = (int) (Math.random() * (ESTATURA_MAXIMA - ESTATURA_MINIMA + 1)) + ESTATURA_MINIMA;
+
+                ESTATURAS[pais][i] = aleatorio;
+
+                System.out.printf("%3d ", aleatorio);
+
+                suma += aleatorio;
+                minima = Math.min(minima, aleatorio);
+                maxima = Math.max(maxima, aleatorio);
+            }
+
+            int media = suma / ESTATURAS_DEPORTISTAS;
+
+            System.out.printf("| %3d %3d %3d", media, minima, maxima);
+
+            System.out.println();
+        }
     }
 }
