51d7a8c3db7c54904bbf39a5671fe204e954d74b
YAGO <yagotortosa72@gmail.com>
2025-12-15 21:52:50 +0100

    kk

--- UD6_2/src/main/java/CONJUNTOS/EJ5.java
+++ UD6_2/src/main/java/CONJUNTOS/EJ5.java
@@ -1,5 +1,6 @@
 package CONJUNTOS;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 public class EJ5 {
@@ -7,14 +8,27 @@ public class EJ5 {
         Scanner sc = new Scanner(System.in);
 
         String nombre;
-        // utilizar LinkedHashSet
+        LinkedHashSet<String> nombres = new LinkedHashSet<>();
 
         do {
-            System.out.print("Introduce nombres (escribe 'fin' para terminar): ");
+            System.out.println("Introduce nombres (escribe 'fin' para terminar): ");
             nombre = sc.nextLine();
 
+            if (nombre.equalsIgnoreCase("fin")) {
+                break;
+            }
 
-        } while (nombre.equals("fin"));
+            if (nombre.isEmpty()) {
+                System.out.println("Nombre inválido: no puede estar vacío.");
+            } else if (!nombre.matches("[a-zA-Z]+")) {
+                System.out.println("Nombre inválido: solo se permiten letras.");
+            } else {
+                nombres.add(nombre);
+            }
+
+        } while (true);
+
+        System.out.println("Conjunto de nombres no ordenados: " + nombres);
 
 
 
--- UD6_2/src/main/java/LISTAS/EJ5.java
+++ UD6_2/src/main/java/LISTAS/EJ5.java
@@ -1,7 +1,59 @@
 package LISTAS;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EJ5 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion;
+        //String nombre;
+        //int tlf;
+
+
+        ArrayList<String> contactos = new ArrayList<>();
+
+
+        do {
+            System.out.print("Agenda de Contactos:");
+            System.out.print("\n1. Anadir Contacto");
+            System.out.print("\n2. Ver Contacto");
+            System.out.print("\n3. Buscar Contactos");
+            System.out.print("\n4. Eliminar Contacto");
+            System.out.print("\n5. Salir");
+
+            System.out.print("\nElige una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("\nIntroduce el nombre: ");
+                    String nombre = sc.next();
+                    contactos.add(nombre);
+                    System.out.print("Introduce el telefono: ");
+                    String telefono = sc.next();
+                    contactos.add(telefono);
+                    System.out.print("Contacto anadido.");
+                    break;
+                case 2:
+                    System.out.print("\nContactos: ");
+                    System.out.println("Nombre: " + contactos.get(0) + ", Telefono: " + contactos.get(1));
+                    break;
+                case 3:
+                    System.out.println("Introduce el nombre a buscar: ");
+                    String nombreAbuscar = sc.next();
+                case 4:
+                    System.out.println();
+
+            }
+
+
+
+
+        } while (opcion != 5);
+
+        System.out.println("HAS SALIDO DE LA AGENDA");
 
     }
 }
