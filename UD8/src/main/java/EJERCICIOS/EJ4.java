9546676c83ce11baafb4f6f12d3d013c32cfe7b8
yagtorrev <yagotortosa72@gmail.com>
2026-02-05 13:50:58 +0100

    hola

--- UD8/src/main/java/EJERCICIOS/EJ4.java
+++ UD8/src/main/java/EJERCICIOS/EJ4.java
@@ -12,6 +12,17 @@ public class EJ4 {
         System.out.print("Introduce el tamaño del vector: ");
         int tam = sc.nextInt();
 
+        sc.nextLine();
+
+        if (tam < 0) {
+            System.out.println("Error: El tamaño del vector no puede ser negativo.");
+
+        } else {
+
+            convertirImprimirVector(rellenarVector(tam));
+
+        }
+
 
 
 
--- UD8/src/main/java/FUNCIONES/FuncionCuatro.java
+++ UD8/src/main/java/FUNCIONES/FuncionCuatro.java
@@ -4,27 +4,37 @@ import java.util.*;
 
 
 public class FuncionCuatro {
-    public static void rellenarVector(int tam)  {
+    public static int[] rellenarVector(int tam)  {
         Scanner sc = new Scanner(System.in);
 
         Set<Integer> conjunto = new HashSet<>();
 
-        for (int i = 0; i < tam; i++) {
+        while (conjunto.size() < tam) {
+            try {
+                System.out.print("Introduce número: ");
+                int num = sc.nextInt();
+                conjunto.add(num);
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Error: Índice fuera de los límites del array.");
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Solo se permite introducir números.");
+                sc.nextLine();
+            }
+        }
+
+
+        int [] vector = new int[tam];
+
+        List<Integer> lista = new ArrayList<>(conjunto);
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = lista.get(i);
 
         }
 
 
-
-
-        try {
-
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Error: Índice fuera de los límites del array.");
-        } catch (InputMismatchException e) {
-            System.out.println("Error: Solo se permite introducir números.");
-        }
-
-
+        return vector;
     }
 
 
@@ -34,9 +44,17 @@ public class FuncionCuatro {
 
         List<Integer> lista = new ArrayList<>();
 
-        Set<Integer> conjunto = new HashSet<>(lista);
+        for (int i : vector) {
+            lista.add(i);
+        }
 
-        System.out.println("Conjunto: " + conjunto);
+        /*for (int n : lista) {
+            System.out.print(n + " - ");
+        }*/
+
+        for (int i = 1; i < lista.size(); i++) {
+            System.out.print(lista.get(i) + " - ");
+        }
 
     }
 }
