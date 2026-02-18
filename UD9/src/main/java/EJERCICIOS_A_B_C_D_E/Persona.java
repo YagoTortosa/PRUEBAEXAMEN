5afccccee12e5979edb5fbdc73d621960f84b0c2
YAGO <yagotortosa72@gmail.com>
2026-02-18 12:42:40 +0100

    NUEVO

--- UD9/src/main/java/EJERCICIOS_A_B_C_D_E/Persona.java
+++ UD9/src/main/java/EJERCICIOS_A_B_C_D_E/Persona.java
@@ -1,31 +1,28 @@
 public class Persona {
-    //static final String dni;
-    String nombre;
-    String apellidos;
-    int edad;
+
+    // Atributos
+    private final String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public static final int MAYORIA_EDAD = 18; // static y final
 
     // Constructores
-
-    public Persona(String dni ,String nombre, String apellidos, int edad) {
+    public Persona(String dni, String nombre, String apellidos, int edad) {
         this.dni = dni;
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.edad = edad;
-    }
-
-    public Persona(String nombre, String apellidos, int edad) {
+        if (!validarDNI(dni)) {
+            throw new IllegalArgumentException("DNI no válido");
+        }
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
     }
 
     // Getters y Setters
-
     public String getDni() {
         return dni;
     }
-
-
 
     public String getNombre() {
         return nombre;
@@ -51,24 +48,27 @@ public class Persona {
         this.edad = edad;
     }
 
-    // METODOS
-
+    // Métodos
     public void imprime() {
-        System.out.println("DNI: " + this.dni + " Nombre: " + this.nombre + " Apellidos: " + this.apellidos);
+        System.out.println("DNI: " + dni + " Nombre: " + nombre + " Apellidos: " + apellidos);
     }
 
     public boolean esMayorDeEdad() {
-        return this.edad >= 18;
+        return edad >= MAYORIA_EDAD;  // uso del static final
     }
 
     public boolean esJubilado() {
-        return this.edad >= 65;
+        return edad >= 65;
     }
 
     public int diferenciaEdad(Persona otraPersona) {
-        return Math.abs(this.edad - otraPersona.getEdad());
+        return Math.abs(this.edad - otraPersona.edad);
     }
 
+    public static boolean validarDNI(String dni) {
+        if (!dni.matches("\\d{8}[A-Za-z]")) {
+            return false;
+        }
+        return true;
+    }
 }
-
-
