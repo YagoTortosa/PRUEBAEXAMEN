7f6f8c3b358a33dfd666bc4a83dc363894431bda
YAGO <yagotortosa72@gmail.com>
2026-02-25 13:43:50 +0100

    NUEVO

--- Daw_StockManagement/src/CLASES/Cuenta.java
+++ Daw_StockManagement/src/CLASES/Cuenta.java
@@ -1,7 +1,7 @@
 import java.util.Set;
 
 public class Cuenta {
-    private String codigo;
+    private final String codigo;
     private String dniResponsable;
     private Departamento dpto;
     private double saldo;
@@ -9,6 +9,17 @@ public class Cuenta {
     private Set<Transaccion> transacciones;
 
     public Cuenta(String codigo, String dniResponsable, Departamento dpto, double saldo, Set<Producto> productos, Set<Transaccion> transacciones) {
+        if (codigo == null || codigo.isEmpty())
+            throw new IllegalArgumentException("El código de la cuenta no puede ser nulo o vacío.");
+
+        if (dniResponsable == null || dniResponsable.isEmpty()) // QUEDA VALIDAR EL FORMATO DEL DNI
+            throw new IllegalArgumentException("El DNI del responsable no es válido. Debe tener 8 dígitos seguidos de una letra.");
+
+         if (dpto == null)
+            throw new IllegalArgumentException("El departamento no puede ser nulo.");
+
+         if (saldo < 0)
+            throw new IllegalArgumentException("El saldo no puede ser negativo.");
         this.codigo = codigo;
         this.dniResponsable = dniResponsable;
         this.dpto = dpto;
@@ -19,10 +30,6 @@ public class Cuenta {
 
     public String getCodigo() {
         return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
     }
 
     public String getDniResponsable() {
@@ -64,4 +71,15 @@ public class Cuenta {
     public void setTransacciones(Set<Transaccion> transacciones) {
         this.transacciones = transacciones;
     }
+
+    // METODOS
+
+    public String generarCodigo(String dniResponsable, Departamento dpto) {
+        StringBuilder codigoCuenta = new StringBuilder();
+        codigoCuenta.append("CTA");
+        codigoCuenta.append("-");
+
+        return dniResponsable;
+    }
+
 }
--- Daw_StockManagement/src/CLASES/Departamento.java
+++ Daw_StockManagement/src/CLASES/Departamento.java
@@ -1,3 +1,12 @@
 public enum Departamento {
-    MARKETING, DIRECCION, INFORMATICA, RRHH
+    MARKETING(3500),
+    DIRECCION(15000),
+    INFORMATICA(10000),
+    RRHH(5000);
+
+    private final double saldo;
+
+    Departamento(double saldo) {
+        this.saldo = saldo;
+    }
 }
